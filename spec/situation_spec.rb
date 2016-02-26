72b612b03aa49fbf9fc8c438e5b3bb4ec1b269a0
Martin Bjeldbak Madsen <martinbmadsen@gmail.com>
2016-02-26 19:16:44 +0100

    Move tests from situation_spec to toy_robot_simulator_spec

--- spec/situation_spec.rb
+++ spec/situation_spec.rb
@@ -2,36 +2,6 @@ require 'spec_helper'
 require 'tempfile'
 
 module ToyRobotSimulator
-  context 'method tests' do
-    describe '.parse_command' do
-      before { Robot.send(:public, *Robot.protected_instance_methods) }
-
-      it 'understands placement' do
-        expect(Robot.parse_command('PLACE 2,4,NORTH')).to eq(
-          cmd: :place, x: 2, y: 4, dir: 'north')
-        expect(Robot.parse_command('PLACE 0,0,EAST')).to eq(
-          cmd: :place, x: 0, y: 0, dir: 'east')
-      end
-
-      it 'understands move' do
-        expect(Robot.parse_command('move')).to eq(cmd: :move)
-      end
-
-      it 'understands left' do
-        expect(Robot.parse_command('left')).to eq(cmd: :left)
-      end
-
-      it 'understands right' do
-        expect(Robot.parse_command('right')).to eq(cmd: :right)
-      end
-
-      it 'understands report' do
-        expect(Robot.parse_command('report')).to eq(cmd: :report)
-      end
-    end
-  end
-
-
   describe 'Obeying situations' do
     let(:output_file) { Tempfile.new('test_output.txt').path }
 
--- spec/toy_robot_simulator_spec.rb
+++ spec/toy_robot_simulator_spec.rb
@@ -21,14 +21,6 @@ module ToyRobotSimulator
         expect(robot.facing).to eq(Direction::NORTH)
       end
 
-      describe '.report_s' do
-        let(:robot) { Robot.new(4, 4, Direction::SOUTH) }
-
-        subject { robot.report_s }
-
-        it { is_expected.to eq '4,4,SOUTH' }
-      end
-
       context 'when placing robot in invalid configurations' do
         it 'throws errors' do
           expect { Robot.new( 5,  0, Direction::NORTH) }.to raise_error RuntimeError
@@ -39,6 +31,44 @@ module ToyRobotSimulator
           expect { Robot.new(-1, -1, Direction::NORTH) }.to raise_error RuntimeError
         end
       end
+    end
+
+    context 'method tests' do
+      describe '.report_s' do
+        let(:robot) { Robot.new(4, 4, Direction::SOUTH) }
+
+        subject { robot.report_s }
+
+        it { is_expected.to eq '4,4,SOUTH' }
+      end
+
+      describe '.parse_command' do
+        before { Robot.send(:public, *Robot.protected_instance_methods) }
+
+        it 'understands placement' do
+          expect(Robot.parse_command('PLACE 2,4,NORTH')).to eq(
+                                                                cmd: :place, x: 2, y: 4, dir: 'north')
+          expect(Robot.parse_command('PLACE 0,0,EAST')).to eq(
+                                                               cmd: :place, x: 0, y: 0, dir: 'east')
+        end
+
+        it 'understands move' do
+          expect(Robot.parse_command('move')).to eq(cmd: :move)
+        end
+
+        it 'understands left' do
+          expect(Robot.parse_command('left')).to eq(cmd: :left)
+        end
+
+        it 'understands right' do
+          expect(Robot.parse_command('right')).to eq(cmd: :right)
+        end
+
+        it 'understands report' do
+          expect(Robot.parse_command('report')).to eq(cmd: :report)
+        end
+      end
+
     end
   end
 end
